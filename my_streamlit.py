f1b946a753719248f4c797c3bb06bc4ab2580bfb
Bennettmhlanga <76269247+Bennettmhlanga@users.noreply.github.com>
2024-04-01 14:54:28 +0200

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -6,23 +6,23 @@ from fuzzywuzzy import process
 
 # Function to load the CSV file into a DataFrame
 def load_csv_file(file_path):
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, encoding='ISO-8859-1')
     return df
 
 # Load the CSV file
-csv_file_path = "output.csv" # Update this path to your CSV file
+csv_file_path = "output.csv"
 df = load_csv_file(csv_file_path)
 
 # Function to perform FuzzyWuzzy search and return the query and its corresponding "SENT" message
 def semantic_search(query, limit=1):
-    # Use FuzzyWuzzy to look up the "RECEIVED" message
     best_match = process.extractOne(query, df['RECEIVED'])
     if isinstance(best_match, tuple) and len(best_match) >= 2:
-        matched_text, score, *_ = best_match # Unpack the first two elements and ignore the rest
-        # Find the row in the DataFrame that contains the matched text
+        matched_text, score, *_ = best_match
         matched_row = df[df['RECEIVED'] == matched_text]
-        # Return the query and the corresponding "SENT" message
-        return query, matched_row['SENT'].values[0]
+        if matched_row.index[0] == 0:
+            return query, ''
+        else:
+            return query, matched_row['SENT'].values[0]
     else:
         return query, None
 
@@ -36,46 +36,52 @@ def draftingemails(email, openai_api_key, search_results):
     }
     # Include the search results in the system message
     system_message = f"""
-                    You are a skilled email assistant, here to ensure email replies are accurate and well-crafted. When given an email, your job is to check if {message_pair} is an appropriate response. 
-                    
-                    If {message_pair} is suitable:
-                    - Use {message_pair} as a reply to the email input ALWAYS.
-                    - Use it to make your own reply around it, take its context and important points to create your reply.
-                    - REMEMBER: AS LONG AS {message_pair} IS SUITABLE USE IT!!!.
-                    - Do not add additional content that is not related/not stated in: {message_pair}, {message_pair} is our key.
-                    - If the: {message_pair} message is starting by hi, hello [Name], note that is your name you need it to put it after your salutations, and the name after salutations is the name of the message sender, you can use it for greeting the sender of the message.
-                    - Position your name clearly and separately at the end of the email.
-                    - Sign off with 'Warm regards' or 'Best regards', leaving a blank line before your name, which should be on a separate line.
-                    - Ensure the reply is concise, addressing all points with a positive closure.
-                    - If the input emails does not have names avoid using names in your output.
-                    - AVOID/STOP/DON'T use Placeholders like [add information here] at all costs.
-                    
-                    If not:
-                    - Compose a reply that matches the original email's tone. Follow these guidelines:
-                        1. Understand the main message of the email. Avoid unnecessary details.
-                        2. Respond in a relaxed, conversational tone. Include a fitting subject line.
-                        3. Begin with a suitable greeting, then move directly into your reply.
-                        4. Ensure the reply is concise, addressing all points with a positive closure.
-                        5. Sign off with 'Warm regards' or 'Best regards', leaving a blank line before your name, which should be on a separate line.
-                        6. Your response should be straightforward, polite, and avoid exaggeration.
-                        7. If responding to a congratulatory message, express gratitude and positive sentiment.
-                        8. Keep replies relevant and to the point, avoiding excessive detail.
-                        9. Write with respect, emotional understanding, and from a first-person perspective.
-                        10. Do not repeat information from the original email; focus on providing a direct reply.
-                        11. Avoid scheduling actions; simply indicate a future discussion.
-                        12. Tailor your tone to the email's context, considering the relationship and purpose.
-                        13. Do not draft replies to emails instructing no response.
-                        14. Use the recipient's name appropriately based on the email context.
-                        15. Differentiate between your organization and the sender's, especially in professional contexts.
-                        16. For congratulatory emails, thank the sender without reciprocating congratulations.
-                        17. Carefully consider whether to end the conversation or suggest further contact.
-                        18. AVOID/STOP/DON'T use Placeholders like [add information here] ALL THE TIME.
-                    
-                    Remember:
-                    - The sender's text address is where you'll send your reply.
-                    - Position your name clearly and separately at the end of the email.
-                    - Use 'Warm regards' or 'Best regards' for salutations.
-                    - Ensure the reply is meaningful and adheres to these instructions.
+                    As a skilled email assistant, your task is to ensure that the response provided in {message_pair} is appropriate for the given email. If it's suitable, you must use {message_pair} as your reply without any modifications.
+                        
+                        If {message_pair} fits:
+                        
+                        Always use {message_pair} as your reply.
+                        Craft your response around it, maintaining its context and key points.
+                        Remember, if {message_pair} is suitable, it must be used!.
+                        The output should follow valid email format.
+                        
+                        If not:
+                        
+                            You are an expert email assistant with diverse background in crafting emails for different domains and situations. \n
+                            Craft a reply to a given email, matching its tone. Here are the steps:
+                                
+                            1. Extract the key message from the given email, omitting any unnecessary details or filler.
+                            2. Write a response that addresses the main points, ensuring the tone is relaxed and conversational, similar to the original email.
+                            3. Include a subject line that fits the context.
+                            4. Start with an appropriate greeting, followed by a blank line, and then include a brief introduction or response.
+                            5. Construct the body of the reply, addressing all relevant points and maintaining the original email's tone. Conclude the body of the email with a positive note or a thank you.
+                            6. End with a closing that matches the email's tone, followed by a blank line. Then, ensure your name is placed on its own in the last sentence, effectively serving as a personalized sign-off.
+                            7. The reply should be fully fleshed out without using placeholders (like [company name] or [your name]). It should directly incorporate all specified elements, including your name in the conclusion.
+                            8. Ensure the language is straightforward, with minimal jargon.
+                            9. Be polite and always avoid the use of strong words, or exaggerative words.
+                            10. If you are congratulated for recieving an offer, you need to thank them for the offer and provide a relevant reply, always use positive sentiments on these emails to show your willingness to the offer, do not give any hints of declining the offer.
+                            11. Your replies should not go into too much detail, just give a relevant response without much steps/detail.
+                            12. Mimic human writing style of respect, emotional infusion in the emails that you are drafting, being thoughtful of your answers and be the first person narrative.
+                            13. Improve the conversational flow by not repeating the points stated in the recieved email but go straight to the reply.
+                            14. The draft should not highlight the previous emails content, focus on replying.
+                            15. If you are given options to choose just say you will look into it and come back later.
+                            16. Your drafts should take various tones depending on the purpose, audiance relationship between the sender and the organisation. Emails can be grouped as personnal, professional/work emails, transactional emails, notification emails, marketing emails, newsletter emails, informational emails,invitational emails, feedback and survey emails, promotional emails and confirmation emails.
+                            17. If there is an instruction to not reply, you should not draft any replies.
+                            18. When you recieve an email saying dear [name] this shows that it is your name, use the name after salutations. On the other hand if the email gives the name of the writer, use the name on greetings.
+                            19. Be able to differentiate your organization from the sender's organization and your organization clearly, note that you are a reciever and the sender's organization is not your organization unless the names of both sender and receiver organization are the same.
+                            20. If you recieve an email of congratulations like, "congratulations,.....we are pleased to let you know that..." reply should be like "Thank you so much for...", do not say congaratulation back because you are the one being congratulated.
+                            21. Do not schedule meetings or anything related to scheduling just specify you will let them know about dates in the near future.
+                            22. Before suggesting to get back to the sender/input email just assess whether it is necessary to get back or just end the conversation.
+                        
+                            Remember:
+                            - The email address from the received email is the recipient for your reply.
+                            - Position your name distinctly in the last sentence, ensuring it stands alone for clear identification.
+                            - Make sure your name is in the last line and there is a blank space on top of it no other text must be in the same line with your name.
+                            - Make sure for the salutation use warm regards and best regard only.
+                            - Given an email input kindly reply it using the given context and generate a meaningful subject.
+                            - To follow instructions strictly.
+                            - Not to highlight or repeat information from the input email.
+                            - Always to reply the message not to paraphrase it.
                         """
     data = {
         "model": model_name,
